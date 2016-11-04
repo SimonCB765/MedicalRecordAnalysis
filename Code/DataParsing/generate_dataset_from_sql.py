80fa8ea895609aa9e368cbeecb8d9f6cbc5b6ab6
sb0080 <s.c.bull@surrey.ac.uk>
2016-11-04 11:36:16 +0000

    Corrected age calculations

--- Code/DataParsing/generate_dataset_from_sql.py
+++ Code/DataParsing/generate_dataset_from_sql.py
@@ -146,7 +146,7 @@ def calculate_age(born, comparison=None, isFraction=False):
     :param comparison:  The date to compare the birthday against (or today's date if no date is supplied).
     :type comparison:   datetime.datetime
     :param isFraction:  Whether the age should be returned as a fractional year (e.g. 35.3) or not (e.g. 35). Rounding
-                            is always performed down.
+                            to the nearest year is always performed down.
     :type isFraction:   bool
 
     """
@@ -155,42 +155,39 @@ def calculate_age(born, comparison=None, isFraction=False):
     if not comparison:
         comparison = datetime.datetime.today()
 
-    # Determine if their birthday would have occured already in the year that comparison occurs in. For example, if the
+    # Determine if their birthday would have occurred already in the year that comparison occurs in. For example, if the
     # person was born on March 5th 1970 and the comparison date is May 12th 2010, then there birthday would already have
     # occurred. However, if the comparison is January 12th 2010, then their birthday would not already have occurred.
     birthdayOccurred = (born.month, born.day) < (comparison.month, comparison.day)
 
     # Get the integer number of years.
-    yearsOld = comparison.year - born.year - birthdayOccurred
+    yearsOld = comparison.year - born.year - (not birthdayOccurred)
 
     # Determine days until the person's next birthday.
     if birthdayOccurred:
         # The person's birthday has occurred in the comparison year, so use the year after it to determine their next
         # birthday.
-        if born.month == 2 and born.day == 29:
-            # The person happened to be born on a leap day (February 29th), so their birthday does not 'exist' in the
-            # next year. Therefore, increment the day and month by one as well (to return March 1st).
-            nextBirthday = born.replace(year=comparison.year + 1, month=3, day=1)
-            daysUntilBirthday = nextBirthday - comparison
-        else:
+        try:
             nextBirthday = born.replace(year=comparison.year + 1)
-            daysUntilBirthday = nextBirthday - comparison
+        except ValueError:
+            # The patient was born on February 29th and the current year was a leap year. Therefore their
+            # birthday does not 'exist' in the next year. Instead, pretend their birthday in the next year is March 1st.
+            nextBirthday = born.replace(year=comparison.year + 1, month=3, day=1)
     else:
-        # The person's birthday has not occurred in the comparison year,so use the comparison year to determine their
+        # The person's birthday has not occurred in the comparison year, so use the comparison year to determine their
         # next birthday.
-        if born.month == 2 and born.day == 29:
-            # The person happened to be born on a leap day (February 29th), so their birthday does not 'exist' in the
-            # next year. Therefore, increment the day and month by one as well (to return March 1st).
-            nextBirthday = born.replace(year=comparison.year, month=3, day=1)
-            daysUntilBirthday = nextBirthday - comparison
-        else:
+        try:
             nextBirthday = born.replace(year=comparison.year)
-            daysUntilBirthday = nextBirthday - comparison
+        except ValueError:
+            # The patient was born on February 29th and the current year was a leap year. Therefore their
+            # birthday does not 'exist' in the next year. Instead, pretend their birthday in the next year is March 1st.
+            nextBirthday = born.replace(year=comparison.year, month=3, day=1)
+    daysUntilBirthday = nextBirthday - comparison
 
     # Return the person's age.
     meanSecondsInYear = 365.25 * 24 * 60 * 60
     if isFraction:
-        return yearsOld + (daysUntilBirthday.total_seconds() / meanSecondsInYear)
+        return yearsOld + ((meanSecondsInYear - daysUntilBirthday.total_seconds()) / meanSecondsInYear)
     else:
         return yearsOld
 
